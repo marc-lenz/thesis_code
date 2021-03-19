f52b11630fbca54729859f67910c4caba9f0e424
lenzomania <marc.lenz.math@gmail.com>
2021-03-19 09:34:34 +0100

    added SGD version of LCA

--- Utils.py
+++ Utils.py
@@ -16,6 +16,7 @@ import itertools
 import numpy as np
 from sklearn.metrics.pairwise import cosine_similarity
 from sklearn import linear_model
+import tensorflow as tf
 
 
 def mate_retrieval_score(l1_vecs, l2_vecs):
@@ -33,29 +34,138 @@ def mate_retrieval_score(l1_vecs, l2_vecs):
 
 
 class LCA_Model:
-    def __init__(self, l1_docs, l2_docs, l1_lsi_model, l2_lsi_model):
+    def __init__(self, l1_docs, l2_docs, l1_lsi_model, l2_lsi_model, clf_type = "nn"):
         self.l1_lsi_model = l1_lsi_model
         self.l2_lsi_model = l2_lsi_model
+        self.clf_type = clf_type
         self.train_model(l1_docs, l2_docs, l1_lsi_model, l2_lsi_model)
         
     def train_model(self, l1_docs, l2_docs, l1_lsi_model, l2_lsi_model):
         l1_lsi_vecs = l1_lsi_model.create_embeddings(l1_docs)
         l2_lsi_vecs = l2_lsi_model.create_embeddings(l2_docs)
-        l1_to_l2_clf = linear_model.LinearRegression()
-        l2_to_l1_clf = linear_model.LinearRegression()
-        l1_to_l2_clf.fit(l1_lsi_vecs, l2_lsi_vecs)
-        l2_to_l1_clf.fit(l2_lsi_vecs, l1_lsi_vecs)      
-        self.l1_to_l2_clf = l1_to_l2_clf
-        self.l2_to_l1_clf = l2_to_l1_clf
+        #l1_to_l2_clf = linear_model.SGDRegressor(penalty = "l2")
+        #l2_to_l1_clf = linear_model.SGDRegressor(penalty = "l2")
         
+        if self.clf_type == "linear":
+            l1_to_l2_clf = linear_model.LinearRegression()
+            l2_to_l1_clf = linear_model.LinearRegression()
+            l1_to_l2_clf.fit(l1_lsi_vecs, l2_lsi_vecs)
+            l2_to_l1_clf.fit(l2_lsi_vecs, l1_lsi_vecs)      
+            self.l1_to_l2_clf = l1_to_l2_clf
+            self.l2_to_l1_clf = l2_to_l1_clf
+            
+        if self.clf_type == "nn":
+            #Parameters for NN
+            loss_function = "MSE" #tf.keras.losses.CosineSimilarity() #"MSE"
+            optimizer_f = "adam"
+            callback = tf.keras.callbacks.EarlyStopping(monitor='val_loss', patience=10)
+            dim = self.l1_lsi_model.dimension
+            
+            #First classifier         
+            l1_to_l2_clf = tf.keras.Sequential([
+                tf.keras.layers.Dense(dim, activation= None)]) # kernel_regularizer='l2'
+            
+            l1_to_l2_clf.compile(optimizer = optimizer_f,
+                          loss = loss_function,#tf.keras.losses.CosineSimilarity(),
+                          metrics=['MSE'])
+
+            l1_to_l2_clf.fit(np.asarray(l1_lsi_vecs), np.asarray(l2_lsi_vecs), 
+                             epochs=200, 
+                             validation_data = (np.asarray(l1_lsi_vecs), np.asarray(l2_lsi_vecs)),
+                             #validation_split=0.2,
+                             callbacks = [callback])
+            
+            #Second classifier
+            l2_to_l1_clf = tf.keras.Sequential([
+                tf.keras.layers.Dense(dim, activation= None)]) #, kernel_regularizer='l2'
+            
+            l2_to_l1_clf.compile(optimizer = optimizer_f,
+                          loss= loss_function, #tf.keras.losses.CosineSimilarity(),
+                          metrics=['MSE'])
+
+            l2_to_l1_clf.fit(np.asarray(l2_lsi_vecs), np.asarray(l1_lsi_vecs), 
+                             epochs=200,  
+                             validation_data = (np.asarray(l1_lsi_vecs), np.asarray(l2_lsi_vecs)),
+                             #validation_split=0.2,
+                             callbacks = [callback])          
+            self.l1_to_l2_clf = l1_to_l2_clf
+            self.l2_to_l1_clf = l2_to_l1_clf      
+  
+
+class Multi_LCA_Model:
+    def __init__(self, doc_list, lsi_models, language_labels clf_type = "nn"):
+        
+        self.lsi_model_dict = lsi_models
+        for idxlabel in language_labels:
+            self.lsi_model_dict
+        self.clf_type = clf_type
+        self.train_model(l1_docs, l2_docs, l1_lsi_model, l2_lsi_model)
+        
+    def train_model(self, l1_docs, l2_docs, l1_lsi_model, l2_lsi_model):
+        l1_lsi_vecs = l1_lsi_model.create_embeddings(l1_docs)
+        l2_lsi_vecs = l2_lsi_model.create_embeddings(l2_docs)
+        #l1_to_l2_clf = linear_model.SGDRegressor(penalty = "l2")
+        #l2_to_l1_clf = linear_model.SGDRegressor(penalty = "l2")
+        
+        if self.clf_type == "linear":
+            l1_to_l2_clf = linear_model.LinearRegression()
+            l2_to_l1_clf = linear_model.LinearRegression()
+            l1_to_l2_clf.fit(l1_lsi_vecs, l2_lsi_vecs)
+            l2_to_l1_clf.fit(l2_lsi_vecs, l1_lsi_vecs)      
+            self.l1_to_l2_clf = l1_to_l2_clf
+            self.l2_to_l1_clf = l2_to_l1_clf
+            
+        if self.clf_type == "nn":
+            #Parameters for NN
+            loss_function = "MSE" #tf.keras.losses.CosineSimilarity() #"MSE"
+            optimizer_f = "adam"
+            callback = tf.keras.callbacks.EarlyStopping(monitor='val_loss', patience=10)
+            dim = self.l1_lsi_model.dimension
+            
+            #First classifier         
+            l1_to_l2_clf = tf.keras.Sequential([
+                tf.keras.layers.Dense(dim, activation= None)]) # kernel_regularizer='l2'
+            
+            l1_to_l2_clf.compile(optimizer = optimizer_f,
+                          loss = loss_function,#tf.keras.losses.CosineSimilarity(),
+                          metrics=['MSE'])
+
+            l1_to_l2_clf.fit(np.asarray(l1_lsi_vecs), np.asarray(l2_lsi_vecs), 
+                             epochs=200, 
+                             validation_data = (np.asarray(l1_lsi_vecs), np.asarray(l2_lsi_vecs)),
+                             #validation_split=0.2,
+                             callbacks = [callback])
+            
+            #Second classifier
+            l2_to_l1_clf = tf.keras.Sequential([
+                tf.keras.layers.Dense(dim, activation= None)]) #, kernel_regularizer='l2'
+            
+            l2_to_l1_clf.compile(optimizer = optimizer_f,
+                          loss= loss_function, #tf.keras.losses.CosineSimilarity(),
+                          metrics=['MSE'])
+
+            l2_to_l1_clf.fit(np.asarray(l2_lsi_vecs), np.asarray(l1_lsi_vecs), 
+                             epochs=200,  
+                             validation_data = (np.asarray(l1_lsi_vecs), np.asarray(l2_lsi_vecs)),
+                             #validation_split=0.2,
+                             callbacks = [callback])          
+            self.l1_to_l2_clf = l1_to_l2_clf
+            self.l2_to_l1_clf = l2_to_l1_clf            
+            
     def create_embeddings(self, docs, language="l1"):
 
         if language == "l1":
             l1_embeddings = self.l1_lsi_model.create_embeddings(docs)
-            l2_embeddings = self.l1_to_l2_clf.predict(l1_embeddings)
+            if self.clf_type == "nn":
+                l2_embeddings = self.l1_to_l2_clf.predict(np.asarray(l1_embeddings))
+            elif self.clf_type == "linear":
+                l2_embeddings = self.l1_to_l2_clf.predict(l1_embeddings)
         if language == "l2":
             l2_embeddings = self.l2_lsi_model.create_embeddings(docs)
-            l1_embeddings = self.l2_to_l1_clf.predict(l2_embeddings)
+            if self.clf_type == "nn":
+                l1_embeddings = self.l1_to_l2_clf.predict(np.asarray(l2_embeddings))
+            elif self.clf_type == "linear":
+                l1_embeddings = self.l2_to_l1_clf.predict(l2_embeddings)
         
         l1_embeddings = np.asarray(l1_embeddings)
         l2_embeddings = np.asarray(l2_embeddings)
@@ -89,7 +199,12 @@ class Vector_Lsi_Model:
         corpus_tfidf = tfidf_model[corpus]
         lsi_model = models.LsiModel(corpus_tfidf, 
                             id2word=dictionary, 
-                            num_topics=dimension)  
+                            num_topics=dimension
+                            #onepass True/False determines which algo is used
+                            #onepass = False,
+                            #power_iters = 1,
+                            #extra_samples= 100
+                            )  
         self.dictionary = dictionary
         self.tfidf_model = tfidf_model
         self.lsi_model = lsi_model
